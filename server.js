43174cc1ea8ec3189f9f64ebe7282b2232724fdc
guydahang7 <guydahang7@gmail.com>
2023-06-13 17:45:26 +0300

    capital letter change

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.post("/imageurl", (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(process.env.port||3000, () => {
-  console.log("app is running on port 3000 ${process.env.port}");
+app.listen(process.env.PORT ||3000, () => {
+  console.log(`app is running on port 3000 ${process.env.PORT}`);
 });
